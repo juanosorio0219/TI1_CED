163129a3bc6d37e21f06d9647d23a65c301979d1
Carlos Villegas Ruiz <1006110037@u.icesi.edu.co>
2022-03-31 11:45:04 -0500

    inputs

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,31 @@
 package ui;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static Scanner LECTOR = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("Write the number of buildings");
+		int numBuildings = LECTOR.nextInt();
+		
+		
+		for(int m = 0; m < numBuildings; m++) {
+			System.out.println("Write the building identifier");
+			String idBuilding = LECTOR.nextLine();
+			
+			System.out.println("Write the number of people currently in the building ( " + idBuilding + " )");
+			int numPeople = LECTOR.nextInt();
+			
+			System.out.println("Write the number of floors (Building " + idBuilding + ")");
+			int numFloors = LECTOR.nextInt();
+			
+			System.out.println("Write the number of offices per floor (Building " + idBuilding + ")");
+			int numOffices = LECTOR.nextInt();
+		}
+		
+		
+		
+		
+	}
 }
